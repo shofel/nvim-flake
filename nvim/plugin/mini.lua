bd2f18b57f99fc3473a7e0ac0ac40fcb50d8f495
Slava <visla.vvi@gmail.com>
2024-12-18 19:04:40 +0500

    restore mini plugins

--- nvim/plugin/mini.lua
+++ nvim/plugin/mini.lua
@@ -1,5 +1,9 @@
-
 if vim.g.did_load_mini_plugin then
   return
 end
 vim.g.did_load_mini_plugin = true
+
+require('mini.align').setup()
+require('mini.comment').setup()
+require('mini.pairs').setup()
+require('mini.surround').setup()
