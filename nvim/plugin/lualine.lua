bd3840735815308a1e6e873ddbe0036d7e284571
Slava <visla.vvi@gmail.com>
2024-12-18 17:44:49 +0500

    lualine

--- nvim/plugin/lualine.lua
+++ nvim/plugin/lualine.lua
@@ -29,59 +29,30 @@ end
 
 require('lualine').setup {
   globalstatus = true,
-  sections = {
+  tabline = {
+    lualine_a = { {"mode", fmt = function (str) return str:sub(1, 1) end}, },
+    lualine_b = {"branch", "diff", "diagnostics"},
     lualine_c = {
-      -- nvim-navic
       { navic.get_location, cond = navic.is_available },
     },
+    lualine_x = {},
+    lualine_y = {},
     lualine_z = {
-      -- (see above)
+      {'tabs', mode = 1, max_length = vim.o.columns / 2},
       { extra_mode_status },
     },
   },
   options = {
     theme = 'auto',
+    component_separators = { left = ""; right = ""; };
+    section_separators   = { left = ""; right = ""; };
+    always_divide_middle = true;
+    globalstatus = true;
   },
-  -- Example top tabline configuration (this may clash with other plugins)
-  -- tabline = {
-  --   lualine_a = {
-  --     {
-  --       'tabs',
-  --       mode = 1,
-  --     },
-  --   },
-  --   lualine_b = {
-  --     {
-  --       'buffers',
-  --       show_filename_only = true,
-  --       show_bufnr = true,
-  --       mode = 4,
-  --       filetype_names = {
-  --         TelescopePrompt = 'Telescope',
-  --         dashboard = 'Dashboard',
-  --         fzf = 'FZF',
-  --       },
-  --       buffers_color = {
-  --         -- Same values as the general color option can be used here.
-  --         active = 'lualine_b_normal', -- Color for active buffer.
-  --         inactive = 'lualine_b_inactive', -- Color for inactive buffer.
-  --       },
-  --     },
-  --   },
-  --   lualine_c = {},
-  --   lualine_x = {},
-  --   lualine_y = {},
-  --   lualine_z = {},
-  -- },
-  winbar = {
-    lualine_z = {
-      {
-        'filename',
-        path = 1,
-        file_status = true,
-        newfile_status = true,
-      },
-    },
-  },
-  extensions = { 'fugitive', 'fzf', 'toggleterm', 'quickfix' },
+  sections = {},
+
+  winbar = {},
+  extensions = { 'fzf', 'toggleterm', 'quickfix' },
 }
+
+vim.opt.laststatus = 0
