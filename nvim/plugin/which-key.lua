87ca39cd0a219852c678b7f59cf5af5651de1872
Slava <visla.vvi@gmail.com>
2024-12-24 14:34:27 +0500

    whichkey: clean up the unimpared mappings

--- nvim/plugin/which-key.lua
+++ nvim/plugin/which-key.lua
@@ -1,3 +1,27 @@
 require('which-key').setup {
-  preset = 'helix'
+  preset = 'helix',
+
+  replace = {
+    key = {
+      function(key)
+        return require("which-key.view").format(key)
+      end,
+      -- { "<Space>", "SPC" },
+    },
+    desc = {
+      -- defaults
+      { "<Plug>%(?(.*)%)", "%1" },
+      { "^%+", "" },
+      { "<[cC]md>", "" },
+      { "<[cC][rR]>", "" },
+      { "<[sS]ilent>", "" },
+      { "^lua%s+", "" },
+      { "^call%s+", "" },
+      { "^:%s*", "" },
+      -- unimpaired
+      { "unimpaired%-", "" },
+      { "%-", " " },
+
+    },
+  },
 }
