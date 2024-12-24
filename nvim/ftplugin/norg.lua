99b046f49192756061069c21e012b84f1dba5c50
Slava <visla.vvi@gmail.com>
2024-12-24 17:32:31 +0500

    neorg: fixup mappings: make them buffer only

--- nvim/ftplugin/norg.lua
+++ nvim/ftplugin/norg.lua
@@ -5,41 +5,41 @@ vim.wo.conceallevel = 2
 
 vim.keymap.set('n', '<Return>cm',
   '<Plug>(neorg.looking-glass.magnify-code-block)',
-  {desc = 'code magnify'})
+  {buffer = true; desc = 'code magnify'})
 vim.keymap.set('n', '<Return>id',
   '<Plug>(neorg.tempus.insert-date)',
-  {desc = 'insert date'})
+  {buffer = true; desc = 'insert date'})
 
 -- List
 vim.keymap.set('n', '<Return>li',
   '<Plug>(neorg.pivot.list.invert)',
-  {desc = 'list invert'})
+  {buffer = true; desc = 'list invert'})
 vim.keymap.set('n', '<Return>lt',
   '<Plug>(neorg.pivot.list.toggle)',
-  {desc = 'list toggle'})
+  {buffer = true; desc = 'list toggle'})
 
 -- Tasks
 vim.keymap.set('n', '<Return>ta',
   '<Plug>(neorg.qol.todo-items.todo.task-ambiguous)',
-  {desc = 'mark task as ambiguous'})
+  {buffer = true; desc = 'mark task as ambiguous'})
 vim.keymap.set('n', '<Return>tc',
   '<Plug>(neorg.qol.todo-items.todo.task-cancelled)',
-  {desc = 'mark task as cancelled'})
+  {buffer = true; desc = 'mark task as cancelled'})
 vim.keymap.set('n', '<Return>td',
   '<Plug>(neorg.qol.todo-items.todo.task-done)',
-  {desc = 'mark task as done'})
+  {buffer = true; desc = 'mark task as done'})
 vim.keymap.set('n', '<Return>th',
   '<Plug>(neorg.qol.todo-items.todo.task-on-hold)',
-  {desc = 'mark task as on hold'})
+  {buffer = true; desc = 'mark task as on hold'})
 vim.keymap.set('n', '<Return>ti',
   '<Plug>(neorg.qol.todo-items.todo.task-important)',
-  {desc = 'mark task as important'})
+  {buffer = true; desc = 'mark task as important'})
 vim.keymap.set('n', '<Return>tp',
   '<Plug>(neorg.qol.todo-items.todo.task-pending)',
-  {desc = 'mark task as pending'})
+  {buffer = true; desc = 'mark task as pending'})
 vim.keymap.set('n', '<Return>tr',
   '<Plug>(neorg.qol.todo-items.todo.task-recurring)',
-  {desc = 'mark task as recurring'})
+  {buffer = true; desc = 'mark task as recurring'})
 vim.keymap.set('n', '<Return>tu',
   '<Plug>(neorg.qol.todo-items.todo.task-undone)',
-  {desc = 'mark task as undone'})
+  {buffer = true; desc = 'mark task as undone'})
