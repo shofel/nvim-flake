c723262fd2196918df85fe05d137344fc1fecbe1
Slava <visla.vvi@gmail.com>
2025-02-05 17:28:00 +0500

    snacks: setup terminal and lazygit

--- nvim/plugin/snacks.lua
+++ nvim/plugin/snacks.lua
@@ -1,5 +1,7 @@
-if vim.g.did_load_snacks_config then
-  return
-end
+local snacks = require('snacks')
 
--- local snacks = require('snacks')
+vim.keymap.set('n', '<space>ht', snacks.lazygit.open, {desc = 'lazygit'})
+
+vim.keymap.set('n', '<Plug>(snacks-terminal-toggle)', function () snacks.terminal.toggle('fish') end)
+vim.keymap.set('n', '<space>ta', '1<Plug>(snacks-terminal-toggle)', {desc = 'toggle terminal 1'})
+vim.keymap.set('n', '<space>to', '2<Plug>(snacks-terminal-toggle)', {desc = 'toggle terminal 2'})
