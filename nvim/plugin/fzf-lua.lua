f72cf1d5f718add631a733092fd8414ec0d7e8e1
Slava <visla.vvi@gmail.com>
2025-02-05 13:39:48 +0500

    fzf: desc for the prefix map

--- nvim/plugin/fzf-lua.lua
+++ nvim/plugin/fzf-lua.lua
@@ -1,25 +1,20 @@
-if vim.g.did_load_fzf_plugin then
-  return
-end
-vim.g.did_load_fzf_plugin = true
-
 local fzf = require('fzf-lua')
-local keymap = vim.keymap
 
 fzf.setup({'max-perf'})
 
-keymap.set('n', '<space>/', fzf.blines,            {desc = 'blines'})
-keymap.set('n', '<space>fc', fzf.command_history,  {desc = 'command history'})
-keymap.set('n', '<space>fg', fzf.live_grep_native, {desc = 'live grep native'})
-keymap.set('n', '<space>fh', fzf.helptags,         {desc = 'helptags'})
-keymap.set('n', '<space>fi', fzf.git_files,        {desc = 'git files'})
-keymap.set('n', '<space>fl', fzf.files,            {desc = 'files'})
-keymap.set('n', '<space>fk', fzf.keymaps,          {desc = 'keymaps'})
-keymap.set('n', '<space>fm', fzf.resume,           {desc = 'resume'})
-keymap.set('n', '<space>fn', fzf.builtin,          {desc = 'builtin'})
-keymap.set('n', '<space>fr', fzf.git_branches,     {desc = 'git branches'})
-keymap.set('n', '<space>fs', fzf.git_status,       {desc = 'git status'})
-keymap.set('n', '<space>ft', fzf.tabs,             {desc = 'tabs'})
-keymap.set('n', '<space>fu', fzf.buffers,          {desc = 'buffers'})
+vim.keymap.set('n', '<space>f',  '<nop>',              {desc = 'fzf'})
+vim.keymap.set('n', '<space>fc', fzf.command_history,  {desc = 'command history'})
+vim.keymap.set('n', '<space>fg', fzf.live_grep_native, {desc = 'live grep native'})
+vim.keymap.set('n', '<space>fh', fzf.helptags,         {desc = 'helptags'})
+vim.keymap.set('n', '<space>fi', fzf.git_files,        {desc = 'git files'})
+vim.keymap.set('n', '<space>fl', fzf.files,            {desc = 'files'})
+vim.keymap.set('n', '<space>fk', fzf.keymaps,          {desc = 'keymaps'})
+vim.keymap.set('n', '<space>fm', fzf.resume,           {desc = 'resume'})
+vim.keymap.set('n', '<space>fn', fzf.builtin,          {desc = 'builtin'})
+vim.keymap.set('n', '<space>fr', fzf.git_branches,     {desc = 'git branches'})
+vim.keymap.set('n', '<space>fs', fzf.git_status,       {desc = 'git status'})
+vim.keymap.set('n', '<space>ft', fzf.tabs,             {desc = 'tabs'})
+vim.keymap.set('n', '<space>fu', fzf.buffers,          {desc = 'buffers'})
 
-keymap.set('v', '<space>f', fzf.grep_visual, {desc = 'grep visual'})
+vim.keymap.set('n', '<space>/', fzf.blines,      {desc = 'blines'})
+vim.keymap.set('v', '<space>f', fzf.grep_visual, {desc = 'grep visual'})
